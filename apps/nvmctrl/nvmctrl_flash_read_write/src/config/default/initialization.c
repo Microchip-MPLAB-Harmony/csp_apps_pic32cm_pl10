cfffe0a7eb323e40445dc08af6298458aa09c5b8
Syam Nair <Syam.Nair@microchip.com>
2025-12-15 05:16:12 +0000

    [NVMCTRL] Regenerated demo for nvmctrl__nvmctrl_flash_read_write__pic32cm_pl10_curiosity_nano.X
    MH3C-444

--- apps/nvmctrl/nvmctrl_flash_read_write/src/config/default/initialization.c
+++ apps/nvmctrl/nvmctrl_flash_read_write/src/config/default/initialization.c
@@ -79,10 +79,10 @@
 #pragma config BOOT_GPIOSEL_POL = 0x0
 #pragma config BOOT_GPIOSEL_ODRAIN = 0x0
 #pragma config BOOT_GPIOSEL_SLEWLIM = 0x0
-#pragma config SERNUM0_SERNUM0 = 0x0U
-#pragma config SERNUM1_SERNUM1 = 0x0U
-#pragma config SERNUM2_SERNUM2 = 0x0U
-#pragma config SERNUM3_SERNUM3 = 0x0U
+#pragma config SERNUM0_SERNUM0 = 0xffffffffU
+#pragma config SERNUM1_SERNUM1 = 0xffffffffU
+#pragma config SERNUM2_SERNUM2 = 0xffffffffU
+#pragma config SERNUM3_SERNUM3 = 0xffffffffU
 
 
 
